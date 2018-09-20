83873e9a11c3bab5e1a988bb3dc39121a094f296
Siamashka Vlad <wladsemashko@gmail.com>
2018-09-20 11:32:49 +0200

    Block header comment

--- hello_world.c
+++ hello_world.c
@@ -1,3 +1,14 @@
+/*----------------------------------------------------------
+ *				HTBLA-Leonding / Class: <your class>
+ * ---------------------------------------------------------
+ * Exercise Number: 1
+ * Title:			Hello World
+ * Author:			Vlad Siamashka
+ * ----------------------------------------------------------
+ * Description:
+ Prints Hello World
+ * ----------------------------------------------------------
+ */
 #include <stdio.h>
 
 int main(int argc, char const *argv[]) {
